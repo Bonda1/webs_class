17eacb75b0068eb2d31ad1fb0b9eeca9a72d2471
bonda1 <qnddj310@gmail.com>
2022-01-17 09:52:37 +0900

    2022.01.17

---
+++ javascript/assets/js/script.js
@@ -0,0 +1,6 @@
+//결과보기
+//제목을 클릭했을 때 ~~~~ 실행하라 !
+document.querySelector(".result h4").addEventListener("click", function(){
+    //alert("클릭했음");
+    document.querySelector(".result01").style.display = "block";
+})
