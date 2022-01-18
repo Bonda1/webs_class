b62adb17a4ddfa133c16505d27ba7c03f3c7300e
Bonda1 <qnddj310@gmail.com>
2022-01-18 21:46:38 +0900

    2022.01.18

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,8 +1,10 @@
 //결과보기 버튼을 클릭하면 경고창을 띄워주세요
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-        alert("ddd") //여기에 토글클래스주면 해결
-    });
+       document.querySelectorAll(".result").forEach(function(el){
+           el.classList.toggle("click");    //토글클래스 사용
+       })
+    })
 });
 
 // document.querySelector(".result").addEventListener("click," () => {});  //화살표 함수로 나타낸다면 function을 지워주고 화살표를 사용.
