8a81302f3645d93807b049c704bccb605aea5e4e
Bonda1 <qnddj310@gmail.com>
2022-01-19 10:40:19 +0900

    2022.01.19

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,10 +1,8 @@
 //결과보기 버튼을 클릭하면 경고창을 띄워주세요
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-       document.querySelectorAll(".result").forEach(function(el){
-           el.classList.toggle("click");    //토글클래스 사용
-       })
-    })
-});
+        el.classList.toggle("active");
+    });
+})
 
 // document.querySelector(".result").addEventListener("click," () => {});  //화살표 함수로 나타낸다면 function을 지워주고 화살표를 사용.
